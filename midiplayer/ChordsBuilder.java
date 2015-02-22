905d695c44f894a5609f6c3e51efcad72317cc70
Mark Fruman <fruman@iau.uni-frankfurt.de>
2015-02-22 16:46:06 -0500

    started ChordsBuilder ... getting a bit complicated, have to slow down

---
+++ midiplayer/ChordsBuilder.java
@@ -0,0 +1,44 @@
+package midiplayer;
+
+// Swing packages
+import javax.swing.*;
+import java.awt.*;
+
+// For resizable arrays
+import java.util.ArrayList;
+
+
+class ChordsBuilder extends JDialog implements actionListener {
+
+    public void actionPerformed(ActionEvent ae) {
+
+    }
+
+    public static void resetChordsBuilderPanel() {
+
+    }
+
+    public static int buildChords(ArrayList<Chord> chords) {
+
+        // build a JPanel with all the chords and buttons
+
+        JPanel mainPanel = new JPanel();
+        for int i
+
+        // chord consists of a name, symbol and JCheckBoxes for notes
+        // relative to the tonic (rather than totally free entry)
+
+
+        // add JPanel to a JScrollPane (in case lots and lots of chords)
+        
+
+        // add JScrollPane to a JOptionPane
+        
+
+        // return "OK=0" or "CANCEL=-1"
+
+    }
+}
+
+
+
